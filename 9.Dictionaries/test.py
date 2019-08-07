11404f234a0ee1865b9e56fd0de69af49fe87e91
Andrei Micuda <andrei.micuda@gmail.com>
2019-08-07 17:28:31 +0300

    Add files via upload

---
+++ 9.Dictionaries/test.py
@@ -0,0 +1,15 @@
+fname = input('Enter file: ')
+
+try: fhandle = open(fname, 'r')
+except:
+	print('No such file.')
+	quit()
+
+emails = dict()
+for line in fhandle:
+	if not line.startswith('From '): continue
+	line = line.split()
+	email = line[1]
+	emails[email]=emails.get(email, 0) + 1
+
+print(emails)
